4ee05167a137285b7955b5e92e4f17fcb9cfde84
Kaleb Garrett <kalebagarrett@gmail.com>
2022-09-20 20:18:02 -0400

    added comments

--- ChefTime/Program/Program.cs
+++ ChefTime/Program/Program.cs
@@ -2,6 +2,14 @@
 
 namespace Program
 {
+// The job of this program is to calculate your meal prep time and
+// give you the remaining time your food has to cook based off your
+// preparation time, the number of minutes it takes to cook your food
+// and how long your food has been cooking for.
+// You should be able to use chef time as much as you want
+// until you decide to quit the loop.
+    
+    
     class Program
     {
         static void Main(string[] args)
